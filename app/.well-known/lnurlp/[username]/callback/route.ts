63fc1b260c3fcb765785fc1752c5b777aad28151
Roland Bewick <roland.bewick@gmail.com>
2025-01-01 17:35:15 +0700

    feat: zaps

--- app/.well-known/lnurlp/[username]/callback/route.ts
+++ app/.well-known/lnurlp/[username]/callback/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest } from "next/server";
 import { findWalletConnection } from "@/app/db";
 import { nwc } from "@getalby/sdk";
+import { validateZapRequest } from "nostr-tools/nip57";
 
 export async function GET(
   request: NextRequest,
@@ -13,6 +14,7 @@ export async function GET(
   const searchParams = request.nextUrl.searchParams;
   const amount = searchParams.get("amount");
   const comment = searchParams.get("comment") || "";
+  const nostr = searchParams.get("nostr") || "";
 
   if (!amount) {
     throw new Error("No amount provided");
@@ -22,11 +24,28 @@ export async function GET(
     username: params.username,
   });
 
+  // subscribe to existing lightning addresses
+  if (!connection.subscribed) {
+    await this.subscribeUser(connection);
+  }
+
   const nwcClient = new nwc.NWCClient({ nostrWalletConnectUrl: connection.id });
+
+  let zapRequest: Event | undefined;
+  if (nostr) {
+    const zapValidationError = validateZapRequest(nostr);
+    if (zapValidationError) {
+      throw new Error(zapValidationError);
+    }
+    zapRequest = JSON.parse(nostr);
+  }
 
   const transaction = await nwcClient.makeInvoice({
     amount: +amount,
     description: comment,
+    metadata: {
+      nostr: zapRequest,
+    },
   });
 
   return Response.json({
